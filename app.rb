58cf8e4998472caa52efcdb7caf7faf955bf03de
Francis <Viviers@atomic.lsd.co.za>
2017-04-07 11:32:31 +0200

    Ruby

--- app.rb
+++ app.rb
@@ -5,6 +5,9 @@ require 'mysql2'
 
 set :port, 8080
 
+$client = Mysql2::Client.new(:host => "172.30.163.225t", :username => "root", :password => "testrootpassword")
+
 get '/' do
-  "Hello world"
+  test = $client.query("show mysq.tables;");
+  "Hello world #{test}"
 end

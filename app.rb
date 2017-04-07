5f581d2d0050fb4af1d0504ff5b9f7e5d946d1a7
Francis <Viviers@atomic.lsd.co.za>
2017-04-07 11:35:37 +0200

    Fix host

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'mysql2'
 
 set :port, 8080
 
-$client = Mysql2::Client.new(:host => "172.30.163.225t", :username => "root", :password => "testrootpassword")
+$client = Mysql2::Client.new(:host => "172.30.163.225", :username => "root", :password => "testrootpassword")
 
 get '/' do
   test = $client.query("show mysq.tables;");

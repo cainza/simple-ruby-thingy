c19de9064802dcc19e3c8b39e01623e51bd993ad
Francis <Viviers@atomic.lsd.co.za>
2017-04-07 12:35:00 +0200

    Re-enable some mariadb

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
 require 'sinatra'
-# require 'mysql2'
+require 'mysql2'
 
 set :port, 8080
 
-#$client = Mysql2::Client.new(:host => "mariadb-test", :username => "root", :password => "testrootpassword")
+$client = Mysql2::Client.new(:host => "mariadb-test", :username => "root", :password => "testrootpassword")
 
 get '/' do
 #  test = $client.query("show mysq.tables;");
